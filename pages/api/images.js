27b777766b7b90bb169dd1367d079299f8a06c3b
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2021-07-19 15:42:58 +0300

    more console logs added

--- pages/api/images.js
+++ pages/api/images.js
@@ -10,11 +10,12 @@ AWS.config.update({
 const s3 = new AWS.S3()
 
 const handler = async (req, res) => {
+
     if (req.method === 'GET') {
-        console.log('Entered the serverless function')
+        console.log('inside function')
         try {
             const images = await Image.find()
-            console.log('connected to db')
+            console.log('got image')
             const imagesWithUrls = images.map(image => {
                 const params = { Bucket: 'nicole-reed-gallery', Key: `${image.userId}/small-${image.fileName}` }
                 const signedUrl = s3.getSignedUrl('getObject', params)
