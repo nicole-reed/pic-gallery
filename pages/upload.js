6fafda25e0888207b0f221b33014dabec40b5b20
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2021-07-26 12:53:44 +0300

    uploader now working on mobile

--- pages/upload.js
+++ pages/upload.js
@@ -3,10 +3,16 @@ import Layout from '../components/layout'
 import { signIn, useSession } from 'next-auth/client'
 import Uppy from '@uppy/core'
 import AwsS3 from '@uppy/aws-s3'
+import { BrowserView, MobileView, isMobile } from 'react-device-detect'
+import { DragDrop } from '@uppy/react'
+import { Dashboard } from '@uppy/react'
+import { ProgressBar } from '@uppy/react'
 import '@uppy/core/dist/style.css'
+import '@uppy/drag-drop/dist/style.css'
+import '@uppy/progress-bar/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
 import axios from 'axios'
-import { Dashboard } from '@uppy/react'
+
 
 
 
@@ -15,7 +21,13 @@ export default function Upload() {
     console.log('session', session)
 
     const uppy = new Uppy({
-        autoProceed: false,
+        // restrictions: {
+        //     maxFileSize: 300000,
+        //     maxNumberOfFiles: 1,
+        //     minNumberOfFiles: 1,
+        //     allowedFileTypes: ['image/*']
+        // },
+        autoProceed: isMobile ? true : false,
         debug: true
     })
         .use(AwsS3, {
@@ -36,7 +48,7 @@ export default function Upload() {
         })
         .on('file-added', () => console.log('file added'))
         .on('upload-success', async (file) => {
-
+            console.log('upload success', file)
             await axios.post(`/api/users/${session.user.id}/images`, { fileName: file.name, contentType: file.type, caption: file.meta.caption })
         })
         .on('upload-error', (error) => console.log('upload error', error))
@@ -55,15 +67,27 @@ export default function Upload() {
                     </>}
                     {session && <>
                         Add new images to {session.user.name}
-                        <div className='uploader'>
-                            <Dashboard
+                        <MobileView>
+                            <DragDrop className='drag-drop' uppy={uppy} />
+                            <ProgressBar
                                 uppy={uppy}
-                                metaFields={[
-                                    { id: 'caption', name: 'Location', placeholder: 'where was this photo taken?' }
-                                ]}
-                                theme={'dark'}
+                                fixed
+                                hideAfterFinish
                             />
-                        </div>
+                        </MobileView>
+                        <BrowserView>
+                            <div className='uploader'>
+                                <Dashboard
+                                    uppy={uppy}
+                                    // width={'300'}
+                                    // height={'350'}
+                                    metaFields={[
+                                        { id: 'caption', name: 'Location', placeholder: 'where was this photo taken?' }
+                                    ]}
+                                    theme={'dark'}
+                                />
+                            </div>
+                        </BrowserView>
 
                     </>}
 
